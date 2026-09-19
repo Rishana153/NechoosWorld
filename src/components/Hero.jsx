b14453d5efb5aa014b9518dc3707dba4e37cad19
Rishana153 <nn5773050@gmail.com>
2026-09-19 13:53:23 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ function Hero() {
               WhatsApp Us
             </a>
             <a
-              href="najeerriyadh@gmail.com"
+              href="mailto:najeerriyadh@gmail.com"
               className="inline-flex rounded-full bg-amber-400 px-6 py-3 text-center font-semibold text-black transition hover:bg-amber-300">
               Email Us
               </a>
